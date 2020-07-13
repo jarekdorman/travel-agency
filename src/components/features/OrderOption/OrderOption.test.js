c042d7cd8697203d2ffc2dfb350120deb221e7b6
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-07-13 10:13:11 -0400

    Test whether comp. displays title delivered via props name

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -12,4 +12,14 @@ describe('Component OrderOption', () => {
     const component = shallow(<OrderOption />);
     expect(component).toEqual({});
   });
+  it('should display title delivered via props name', () => {
+    const expectedTitle = 'The title';
+    const component = shallow(
+      <OrderOption name={expectedTitle} type="number" />
+    );
+
+    expect(component.find('.title').text()).toEqual(expectedTitle);
+    console.log(component.debug());
+  });
+
 });
