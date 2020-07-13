c51739240d850c86965284ea6af54ae0324f6414
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-07-13 10:26:39 -0400

    Test subcomponents, add test case for dropdown type

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -21,5 +21,82 @@ describe('Component OrderOption', () => {
     expect(component.find('.title').text()).toEqual(expectedTitle);
     console.log(component.debug());
   });
-
 });
+
+const optionTypes = {
+  dropdown: 'OrderOptionDropdown',
+  icons: 'OrderOptionIcons',
+  checkboxes: 'OrderOptionCheckboxes',
+  number: 'OrderOptionNumber',
+  text: 'OrderOptionText',
+  date: 'OrderOptionDate',
+};
+
+const mockProps = {
+  id: 'abc',
+  name: 'Lorem',
+  values: [
+    { id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0 },
+    { id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100 },
+  ],
+  required: false,
+  currentValue: 'aaa',
+  price: '50%',
+  limits: {
+    min: 0,
+    max: 6,
+  },
+};
+
+const mockPropsForType = {
+  dropdown: {},
+  icons: {},
+  checkboxes: { currentValue: [mockProps.currentValue] },
+  number: { currentValue: 1 },
+  text: {},
+  date: {},
+};
+
+// const testValue = mockProps.values[1].id;
+// const testValueNumber = 3;
+
+for (let type in optionTypes) {
+  describe(`Component OrderOption with type=${type}`, () => {
+    /* test setup */
+    let component;
+    let subcomponent;
+    let renderedSubcomponent;
+
+    beforeEach(() => {
+      component = shallow(
+        <OrderOption type={type} {...mockProps} {...mockPropsForType[type]} />
+      );
+      subcomponent = component.find(optionTypes[type]);
+      renderedSubcomponent = subcomponent.dive();
+    });
+    /* common tests */
+    it(`renders ${optionTypes[type]}`, () => {
+      expect(subcomponent).toBeTruthy();
+      expect(subcomponent.length).toBe(1);
+    });
+    /* type-specific tests */
+    switch (type) {
+      case 'dropdown': {
+        /* tests for dropdown */
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('select');
+          expect(select.length).toBe(1);
+
+          const emptyOption = select.find('option[value=""]').length;
+          expect(emptyOption).toBe(1);
+
+          const options = select.find('option').not('[value=""]');
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+        break;
+      }
+    }
+  });
+}
