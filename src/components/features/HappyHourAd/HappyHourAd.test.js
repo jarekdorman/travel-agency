00f3845aad7b799e614556234e636627fecba446
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-07-15 15:55:18 -0400

    Prepare title & description for HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  description: '.promoDescription',
+};
+
 describe('Component HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+  it('should render header and description', () => {
+    const component = shallow(<HappyHourAd />);
+
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.description)).toEqual(true);
   });
 });
