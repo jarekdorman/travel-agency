6d8443ebf60e42321e193191508e36795125efe3
Jaroslaw Jay Dorman <jay@jaydorman.com>
2020-07-16 09:09:34 -0400

    Re-render component every one second

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -2,24 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
+  constructor() {
+    super();
+
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+
   static propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
   };
 
-  getCountdownTime () {
+  getCountdownTime() {
     const currentTime = new Date();
-    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+    const nextNoon = new Date(
+      Date.UTC(
+        currentTime.getUTCFullYear(),
+        currentTime.getUTCMonth(),
+        currentTime.getUTCDate(),
+        12,
+        0,
+        0,
+        0
+      )
+    );
 
-
-    if(currentTime.getUTCHours() >=12) {
+    if (currentTime.getUTCHours() >= 12) {
       nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
     }
-    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
   }
 
   render() {
-    const { title} = this.props;
+    const { title } = this.props;
     return (
       <div>
         <h3 className={'title'}>{title}</h3>
